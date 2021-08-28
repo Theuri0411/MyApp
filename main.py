182930bf83dade03f4157a31cfd44e737bda818f
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-29 01:05:48 +0300

    added labels for app

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 
+
 class childApp(GridLayout):
     def __init__(self, **kwargs):
         super(childApp, self).__init__()
         self.cols = 2
-        self.add_widget(Label)
+        self.add_widget(Label(text="Student Name"))
+        self.s_name= TextInput()
+        self.add_widget(self.s_name)
+
+        self.add_widget(Label(text="Student Marks"))
+        self.s_marks = TextInput()
+        self.add_widget(self.s_marks)
+
+        self.add_widget(Label(text="Student Gender"))
+        self.s_gender = TextInput()
+        self.add_widget(self.s_gender)
+
+        self.add_widget(Label(text="Student Age"))
+        self.s_age = TextInput()
+        self.add_widget(self.s_age)
